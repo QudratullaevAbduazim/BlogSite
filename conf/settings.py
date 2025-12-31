d1dd4d89a921f4c138c4816e2a82f38fdf16abb6
QudratullaevAbduazim <abduazimqudratullaev@gmail.com>
2025-12-31 14:41:16 +0500

    full

--- conf/settings.py
+++ conf/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
