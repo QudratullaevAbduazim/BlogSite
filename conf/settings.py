14ff91950d6ba0c787fcca40343ef56d49a7379e
QudratullaevAbduazim <abduazimqudratullaev@gmail.com>
2026-01-01 11:24:49 +0500

    add django-jazzmin

--- conf/settings.py
+++ conf/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = [
 # APPLICATIONS
 # ======================
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'jazzmin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
